b26d60e8f102482095a5bc278f123b05098d6ffe
quinfer <b.quinn@qub.ac.uk>
2022-02-11 05:26:08 +0000

    05 slide deck ported

--- teaching admin.R
+++ teaching admin.R
@@ -1,17 +1,25 @@
 library(rio)
 library(googledrive)
+library(usethis)
 # install.packages("remotes")
 #remotes::install_github("rundel/ghclass")
 library(ghclass)
 #drive_find("github")
 #install_formats()
-setwd("~/Dropbox/Teaching")
+#setwd("~/Dropbox/Teaching")
 drive_download("github student information",overwrite = TRUE)
 
 # list.files()
 # import('FIN7028 emails.xls')->ids
 students_info<-import("github student information.xlsx")
 students<-students_info$`Github Username`[!is.na(students_info$`Github Username`)]
-org_invite(org = "Time-series-financial-econometrics",
-           user = students)
-students
+org_admins(org = "Time-series-financial-econometrics")
+org_repos(org = "Time-series-financial-econometrics")
+org_members(org = "Time-series-financial-econometrics")
+
+org_invite(org = "Time-series-financial-econometrics",user = students)
+
+gh::gh_whoami()
+# usethis::browse_github()
+# usethis::create_github_token()
+credentials::set_github_pat()
